3abfb576f9f9c6b4bf51c0c6d2ab58ba865b5e4b
Niko Heikkilä <niko.heikkila@futurice.com>
2021-11-16 15:16:35 +0200

    feat: add toHaveLeftValue() matcher

--- src/matchers/Either/index.test.ts
+++ src/matchers/Either/index.test.ts
@@ -4,22 +4,30 @@ import * as matchers from "./";
 expect.extend(matchers);
 
 describe("Either", () => {
-  const left = () => Left(new Error("Message"));
-  const right = () => Right(1);
+    const left = () => Left("Error");
+    const right = () => Right(1);
 
-  it(".toBeLeft", () => {
-    expect(left()).toBeLeft();
-  });
+    it(".toBeLeft", () => {
+        expect(left()).toBeLeft();
+    });
 
-  it(".not.toBeLeft", () => {
-    expect(right()).not.toBeLeft();
-  });
+    it(".not.toBeLeft", () => {
+        expect(right()).not.toBeLeft();
+    });
 
-  it(".toBeRight", () => {
-      expect(right()).toBeRight();
-  });
+    it(".toBeRight", () => {
+        expect(right()).toBeRight();
+    });
 
-  it(".not.toBeRight", () => {
-      expect(left()).not.toBeRight();
-  });
+    it(".not.toBeRight", () => {
+        expect(left()).not.toBeRight();
+    });
+
+    it(".toHaveLeftValue", () => {
+        expect(left()).toHaveLeftValue("Error");
+    });
+
+    it(".not.toHaveLeftValue", () => {
+        expect(right()).not.toHaveLeftValue(1);
+    });
 });
--- src/matchers/Either/index.ts
+++ src/matchers/Either/index.ts
@@ -1,13 +1,14 @@
-import { Either } from "purify-ts";
+import { Either, Left } from "purify-ts";
 import { fail, pass } from "../../utils";
 
 type U = unknown;
 
 export function toBeLeft(actual: Either<U, U>) {
-  return actual.caseOf({
-    Left: (_) => pass(`Expected value not to be Left, but got Left instead`),
-    Right: (_) => fail(`Expected value to be Left, but got Right instead`),
-  });
+    return actual.caseOf({
+        Left: (_) =>
+            pass(`Expected value not to be Left, but got Left instead`),
+        Right: (_) => fail(`Expected value to be Left, but got Right instead`),
+    });
 }
 
 export function toBeRight(actual: Either<U, U>) {
@@ -16,4 +17,12 @@ export function toBeRight(actual: Either<U, U>) {
         Right: (_) =>
             pass(`Expected value not to be Right, but got Right instead`),
     });
+}
+
+export function toHaveLeftValue<T>(actual: Either<T, U>, value: T) {
+    return actual.equals(Left(value))
+        ? pass(`Did not expect ${actual.toString()}, but got it instead`)
+        : fail(
+              `Expected Left with value ${value}, but got ${actual.toString()} instead`
+          );
 }
